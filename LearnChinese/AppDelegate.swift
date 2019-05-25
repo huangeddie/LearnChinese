7e53025702391541b62936664b68ab6d3e957d5c
Eddie Huang <aigagror@gmail.com>
2019-05-25 18:19:47 -0400

    added notifications

--- LearnChinese/AppDelegate.swift
+++ LearnChinese/AppDelegate.swift
@@ -8,15 +8,29 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let center = UNUserNotificationCenter.current()
+        // Request permission to display alerts and play sounds.
+        center.requestAuthorization(options: [.alert, .sound, .badge])
+        { (granted, error) in
+            // Enable or disable features based on authorization.
+            if !granted {
+                return
+            }
+            
+            self.createNewEntryNotification()
+            self.createReviewEntryNotification()
+        }
+        
         let userDefaults = UserDefaults.standard
         
         if !userDefaults.bool(forKey: "launchedBefore") {
@@ -68,6 +82,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -121,6 +137,68 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+    // MARK: - Notifications
+    private func createNewEntryNotification() {
+        // Notifications
+        let content = UNMutableNotificationContent()
+        content.title = "Time for a New Entry"
+        content.subtitle = "新的中文短语"
+        content.badge = 1
+        
+        var dateComponents = DateComponents()
+        dateComponents.calendar = Calendar.current
+        
+        //            dateComponents.weekday = 1  // Sunday
+        //            dateComponents.hour = 12    // 12:00 hours
+        dateComponents.second = 0
+        
+        // Create the trigger as a repeating event.
+        let trigger = UNCalendarNotificationTrigger(
+            dateMatching: dateComponents, repeats: true)
+        
+        // Create the request
+        let request = UNNotificationRequest(identifier: "new-entry",
+                                            content: content, trigger: trigger)
+        
+        // Schedule the request with the system.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+            if error != nil {
+                // Handle any errors.
+            }
+        }
+    }
+    
+    private func createReviewEntryNotification() {
+        // Notifications
+        let content = UNMutableNotificationContent()
+        content.title = "Time for review"
+        content.subtitle = "复习中文"
+        content.badge = 1
+        
+        var dateComponents = DateComponents()
+        dateComponents.calendar = Calendar.current
+        
+        dateComponents.weekday = 5  // Thursday
+        dateComponents.hour = 12    // 12:00 hours
+        
+        // Create the trigger as a repeating event.
+        let trigger = UNCalendarNotificationTrigger(
+            dateMatching: dateComponents, repeats: true)
+        
+        // Create the request
+        let request = UNNotificationRequest(identifier: "review-entry",
+                                            content: content, trigger: trigger)
+        
+        // Schedule the request with the system.
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+            if error != nil {
+                // Handle any errors.
             }
         }
     }
